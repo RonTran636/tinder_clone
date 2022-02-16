39115b6a8b84533eb88fb2cc0f61dc961b31d143
Minh Tran <minh.tran@saigontechonlogy.com>
2022-02-16 21:48:41 +0700

    - Add navigator to list_user_screen

--- lib/bloc/home/home_bloc.dart
+++ lib/bloc/home/home_bloc.dart
@@ -12,17 +12,17 @@ part 'home_state.dart';
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   final _apiHelper = ApiHelper();
   late final List<User> _listUser;
-  final _likedListUser = <User>[];
-  final _passedListUser = <User>[];
+  final likedListUser = <User>[];
+  final passedListUser = <User>[];
+
   HomeBloc() : super(const HomeState.initial()) {
     on<HomeLoadInitialList>(_onLoadInitialList);
     on<HomeLikeUser>(_onLikeUser);
     on<HomePassUser>(_onPassUser);
-    on<HomePassedList>(_onPassedListTapped);
-    on<HomeLikedList>(_onLikedListTapped);
   }
 
-  void _onLoadInitialList(HomeLoadInitialList event, Emitter<HomeState> emit) async {
+  void _onLoadInitialList(
+      HomeLoadInitialList event, Emitter<HomeState> emit) async {
     emit(const HomeState.loading());
     await _apiHelper.getListUser().then((value) {
       _listUser = value;
@@ -30,31 +30,25 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
     }).whenComplete(() => emit(HomeState.loaded(userList: _listUser)));
   }
 
-  void _onLikeUser(HomeLikeUser event, Emitter<HomeState> emit) {
-    print("Like user called");
-    emit(HomeState.loading());
-    //Perform animation like
-
+  void _onLikeUser(HomeLikeUser event, Emitter<HomeState> emit) async {
+    emit(const HomeState.loading());
     //Add user to liked list
-    _likedListUser.add(event.user);
+    likedListUser.add(event.user);
     //Remove user from parent list
     _listUser.remove(event.user);
-    emit(HomeState.loaded(userList: _listUser));
+    await Future.delayed(const Duration(milliseconds: 500), () {}).whenComplete(
+      () => emit(HomeState.loaded(userList: _listUser)),
+    );
   }
 
-  void _onPassUser(HomePassUser event, Emitter<HomeState> emit) {
-    print("Unlike user called");
-    emit(HomeState.loading());
-    //Perform animation pass
-
+  void _onPassUser(HomePassUser event, Emitter<HomeState> emit) async {
+    emit(const HomeState.loading());
     //Add user to passed list
-    _passedListUser.add(event.user);
+    passedListUser.add(event.user);
     //Remove user from parent list
     _listUser.remove(event.user);
-    emit(HomeState.loaded(userList: _listUser));
+    await Future.delayed(const Duration(milliseconds: 500), () {}).whenComplete(
+      () => emit(HomeState.loaded(userList: _listUser)),
+    );
   }
-
-  void _onPassedListTapped(HomePassedList event, Emitter<HomeState> emit) {}
-
-  void _onLikedListTapped(HomeLikedList event, Emitter<HomeState> emit) {}
 }
--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -2,10 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tinder_clone/bloc/home/home_bloc.dart';
+import 'package:tinder_clone/data/model/user/user.dart';
 import 'package:tinder_clone/utils/assets.gen.dart';
 import 'package:tinder_clone/utils/colors.dart';
 import 'package:tinder_clone/utils/dimensions.dart';
 import 'package:tinder_clone/utils/fonts.dart';
+import 'package:tinder_clone/views/screens/list_user_screen.dart';
 import 'package:tinder_clone/views/widgets/rounded_button.dart';
 import 'package:tinder_clone/views/widgets/tinder_card.dart';
 
@@ -17,7 +19,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,98 +33,20 @@ class _HomeScreenState extends State<HomeScreen> {
               children: [
                 SizedBox(height: AppDimen.defaultMargin),
                 Expanded(
-                  child: TinderCard(
-                    firstName: listUser[0].firstName,
-                    imageUrl: listUser[0].picture,
+                  child: Stack(
+                    children: listUser.reversed
+                        .map(
+                          (value) => TinderCard(
+                            firstName: value.firstName,
+                            imageUrl: value.picture,
+                          ),
+                        )
+                        .toList(),
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    RoundedButton(
-                      iconPath: Assets.icons.clear.path,
-                      color: AppColor.buttonBackground,
-                      iconColor: AppColor.appRedColor,
-                      onPressed: () => context.read<HomeBloc>()
-                        ..add(
-                          HomeEvent.passUser(listUser[0]),
-                        ),
-                    ),
-                    SizedBox(width: AppDimen.defaultMargin / 2),
-                    RoundedButton(
-                      iconPath: Assets.icons.star.path,
-                      color: AppColor.buttonBackground,
-                      iconColor: AppColor.appBlue,
-                      onPressed: () {},
-                    ),
-                    SizedBox(width: AppDimen.defaultMargin / 2),
-                    RoundedButton(
-                      iconPath: Assets.icons.heart.path,
-                      color: AppColor.buttonBackground,
-                      iconColor: AppColor.appLikeColor,
-                      onPressed: () => context.read<HomeBloc>()
-                        ..add(
-                          HomeEvent.likeUser(listUser[0]),
-                        ),
-                    ),
-                  ],
-                ),
+                _actionButtons(listUser),
                 SizedBox(height: AppDimen.defaultMargin),
-                Container(
-                  color: AppColor.background,
-                  padding: EdgeInsets.all(16.sz),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Flexible(
-                        flex: 1,
-                        child: InkWell(
-                          onTap: () {},
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              const Icon(
-                                Icons.edit_off,
-                                color: AppColor.appRedColor,
-                              ),
-                              SizedBox(width: 8.sz),
-                              Text(
-                                "Second Look",
-                                style: TextStyle(
-                                  fontSize: AppFont.fontSizeSmall,
-                                  color: AppColor.textColor,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Flexible(
-                        flex: 1,
-                        child: InkWell(
-                          onTap: () {},
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              const Icon(
-                                CupertinoIcons.heart_solid,
-                                color: AppColor.appRedColor,
-                              ),
-                              SizedBox(width: 8.sz),
-                              Text(
-                                "Liked List",
-                                style: TextStyle(
-                                  fontSize: AppFont.fontSizeSmall,
-                                  color: AppColor.textColor,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                _listButton(),
                 SizedBox(height: 16.sz)
               ],
             ),
@@ -143,5 +66,115 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
+  }
+
+  Widget _actionButtons(List<User> listUser) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        RoundedButton(
+          iconPath: Assets.icons.clear.path,
+          color: AppColor.buttonBackground,
+          iconColor: AppColor.appRedColor,
+          onPressed: () => context.read<HomeBloc>()
+            ..add(
+              HomeEvent.passUser(listUser[0]),
+            ),
+        ),
+        SizedBox(width: AppDimen.defaultMargin / 2),
+        RoundedButton(
+          iconPath: Assets.icons.star.path,
+          color: AppColor.buttonBackground,
+          iconColor: AppColor.appBlue,
+          onPressed: () {},
+        ),
+        SizedBox(width: AppDimen.defaultMargin / 2),
+        RoundedButton(
+          iconPath: Assets.icons.heart.path,
+          color: AppColor.buttonBackground,
+          iconColor: AppColor.appLikeColor,
+          onPressed: () => context.read<HomeBloc>()
+            ..add(
+              HomeEvent.likeUser(listUser[0]),
+            ),
+        ),
+      ],
+    );
+  }
+
+  Widget _listButton() {
+    return Container(
+      color: AppColor.background,
+      padding: EdgeInsets.all(16.sz),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Flexible(
+            flex: 1,
+            child: InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ListUserScreen(
+                      listUser: context.read<HomeBloc>().passedListUser, tag: "Second Look",
+                    ),
+                  ),
+                );
+              },
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(
+                    Icons.edit_off,
+                    color: AppColor.appRedColor,
+                  ),
+                  SizedBox(width: 8.sz),
+                  Text(
+                    "Second Look",
+                    style: TextStyle(
+                      fontSize: AppFont.fontSizeSmall,
+                      color: AppColor.textColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ListUserScreen(
+                      listUser: context.read<HomeBloc>().likedListUser, tag: "Liked List",
+                    ),
+                  ),
+                );
+              },
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(
+                    CupertinoIcons.heart_solid,
+                    color: AppColor.appRedColor,
+                  ),
+                  SizedBox(width: 8.sz),
+                  Text(
+                    "Liked List",
+                    style: TextStyle(
+                      fontSize: AppFont.fontSizeSmall,
+                      color: AppColor.textColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
